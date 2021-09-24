7f3c33ebb856001a87b5acae430be43bec8dfe61
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-09-24 03:47:53 -0700

    Agregado estilos a la tabla del clima

--- src/app/components/weatherInfo.js
+++ src/app/components/weatherInfo.js
@@ -26,7 +26,7 @@ const WeatherInfo = props => {
                             </div>
                         </div>
                         <div class="row plan-table">
-                            <div class="col-lg-8 text-white col-md-6">
+                            <div class="col-lg-7 text-white col-md-6">
                                 <div class="plan-style blue-shadow">
                                     <div class="plan-style-bg blue-bg-imh"></div>
                                     <div class="plan-header">
@@ -35,9 +35,73 @@ const WeatherInfo = props => {
                                     <div class="plan-detail">
                                         <ul>
                                             <li>Temperature: {props.temperature[12]} °C</li>
-                                            <li>Time: {props.time[12].slice(11, 16)}</li>
+                                            <li>Time: {props.time[12].slice(11, 16)}pm</li>
                                             <li>Elevation: {props.elevation}</li>
                                             <li>Wind Speed: {props.windspeed[1]}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 text-white col-md-6">
+                                <div class="plan-style orange-shadow active">
+                                    <div class="plan-style-bg orange-bg-imh"></div>
+                                    <div class="plan-header">
+                                        <h3 class="bg-orange">{props.time[36].slice(0,10)}</h3>
+                                    </div>
+                                    <div class="plan-detail">
+                                        <ul>
+                                            <li>Temperature: {props.temperature[36]} °C</li>
+                                            <li>Time: {props.time[36].slice(11, 16)}pm</li>
+                                            <li>Elevation: {props.elevation}</li>
+                                            <li>Wind Speed: {props.windspeed[2]}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 text-white col-md-6">
+                                <div class="plan-style green-shadow active">
+                                    <div class="plan-style-bg green-bg-imh"></div>
+                                    <div class="plan-header">
+                                        <h3 class="bg-green">{props.time[60].slice(0,10)}</h3>
+                                    </div>
+                                    <div class="plan-detail">
+                                        <ul>
+                                            <li>Temperature: {props.temperature[60]} °C</li>
+                                            <li>Time: {props.time[60].slice(11, 16)}pm</li>
+                                            <li>Elevation: {props.elevation}</li>
+                                            <li>Wind Speed: {props.windspeed[3]}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 text-white col-md-6">
+                                <div class="plan-style yellow-shadow active">
+                                    <div class="plan-style-bg yellow-bg-imh"></div>
+                                    <div class="plan-header">
+                                        <h3 class="bg-yellow">{props.time[84].slice(0,10)}</h3>
+                                    </div>
+                                    <div class="plan-detail">
+                                        <ul>
+                                            <li>Temperature: {props.temperature[84]} °C</li>
+                                            <li>Time: {props.time[84].slice(11, 16)}pm</li>
+                                            <li>Elevation: {props.elevation}</li>
+                                            <li>Wind Speed: {props.windspeed[4]}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 text-white col-md-6">
+                                <div class="plan-style red-shadow active">
+                                    <div class="plan-style-bg red-bg-imh"></div>
+                                    <div class="plan-header">
+                                        <h3 class="bg-red">{props.time[108].slice(0,10)}</h3>
+                                    </div>
+                                    <div class="plan-detail">
+                                        <ul>
+                                            <li>Temperature: {props.temperature[108]} °C</li>
+                                            <li>Time: {props.time[108].slice(11, 16)}pm</li>
+                                            <li>Elevation: {props.elevation}</li>
+                                            <li>Wind Speed: {props.windspeed[5]}</li>
                                         </ul>
                                     </div>
                                 </div>
